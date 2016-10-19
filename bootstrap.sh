a1be4f2ca8dca18104c3b40ffdd003f17132c859
Jun QIN <jun.qin@amadeus.com>
2016-10-19 23:37:40 +0200

    add code to wait apt/dpkg to finish, curl -s -S

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+echo -n "Waiting for ongoing apt/dpkg processes to be finished "
+while true; do
+	ps -ef | egrep "/usr/bin/dpkg|/usr/lib/apt/apt.systemd.daily" | grep -v grep >/dev/null
+	if [ $? -ne 0 ]; then
+		break
+	fi
+	echo -n .
+	sleep 3
+done
+echo
 
 # install puppet collection repo
 if [ ! -f /etc/apt/sources.list.d/puppetlabs-pc1.list ]; then
 	repodeb=puppetlabs-release-pc1-xenial.deb
-	curl https://apt.puppetlabs.com/$repodeb -o $repodeb
+	curl -s -S https://apt.puppetlabs.com/$repodeb -o $repodeb
 	dpkg -i $repodeb
 fi
 
